58760ba719f071b465c2d07d613b8966dc42810b
Manuel Ulate <fr3zs09@gmail.com>
2022-11-27 17:46:59 -0600

    creacion de rutas en router

---
+++ src/app/app-router.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { BasicsComponent } from './sales/pages/basics/basics.component';
+import { NoCommonsComponent } from './sales/pages/no-commons/no-commons.component';
+import { NumbersComponent } from './sales/pages/numbers/numbers.component';
+import { SortedComponent } from './sales/pages/sorted/sorted.component';
+
+const routes : Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: BasicsComponent
+  },
+  {
+    path: 'numbers',
+    component: NumbersComponent
+  },
+  {
+    path: 'no-commons',
+    component: NoCommonsComponent
+  },
+  {
+    path: 'sorted',
+    component: SortedComponent
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot([
+
+    ])
+  ]
+})
+export class AppRouterModule { }
---
+++ src/app/sales/pages/sorted/sorted.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sorted',
+  templateUrl: './sorted.component.html'
+})
+export class SortedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
