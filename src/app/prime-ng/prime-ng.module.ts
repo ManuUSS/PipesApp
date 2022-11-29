24fe958852d234eb86883659eb181566d88b90ac
Manuel Ulate <fr3zs09@gmail.com>
2022-11-29 12:39:34 -0600

    pipes no comunes y fieldset

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { MenubarModule } from 'primeng/menubar';
-import {CardModule} from 'primeng/card';
-
+import { CardModule } from 'primeng/card';
+import { FieldsetModule } from 'primeng/fieldset';
 
 @NgModule({
   exports: [
     CardModule,
+    FieldsetModule,
     MenubarModule
   ]
 })
