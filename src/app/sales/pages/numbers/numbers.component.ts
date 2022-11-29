72ce14334abbdeaacad2d283624a88835312f51b
Manuel Ulate <fr3zs09@gmail.com>
2022-11-29 12:22:24 -0600

    pipe decimal

--- src/app/sales/pages/numbers/numbers.component.ts
+++ src/app/sales/pages/numbers/numbers.component.ts
@@ -1,14 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-numbers',
   templateUrl: './numbers.component.html'
 })
-export class NumbersComponent implements OnInit {
+export class NumbersComponent {
 
-  constructor() { }
+  public sales: number = 123456789.12345;
+  public percentage: number = 0.4856;
 
-  ngOnInit(): void {
-  }
 
 }
